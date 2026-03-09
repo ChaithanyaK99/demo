074b025820d4ec1dce5752b8c16b950178ef453c
ChaithanyaK99 <you@example.com>
2026-03-09 21:13:40 +0530

    Added uc2.java

---
+++ uc2.java
@@ -0,0 +1,14 @@
+public class OOPSannerApp{
+Public static void main(String args[])
+{
+ System.out.println(" *     *     **   ** ");
+ System.out.println("*   *   *   *    *   *  *    ");
+ System.out.println("*   *   *   *    *   *  *    ");
+ System.out.println("*   *   *   *    *   *  ** ");
+ System.out.println("*   *   *   *    *   *      *");
+ System.out.println("*   *   *   *    *   *      * ");
+ System.out.println("*   *   *   *    *   *  *** ");
+ 
+ 
+)
+)
